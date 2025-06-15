3188ad58876f62010d39f53f5d81ab19a0c30cef
ElmutKool <41754380+ElmutKool@users.noreply.github.com>
2025-06-15 13:03:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
-  }
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // valikuline, kui tahad kasutada @/
+    },
+  },
+  server: {
+    historyApiFallback: true, // <-- oluline ridade uuesti töötamiseks
+  },
 });
